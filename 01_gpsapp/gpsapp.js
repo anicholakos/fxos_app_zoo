5680dc88266f97d3aef039b6b4453d91372dffcc
Aki Nicholakos <aki42197@gmail.com>
2015-05-11 14:43:26 -0400

    Combined product of many codez. Exact logs lost due to power failure.

---
+++ 01_gpsapp/gpsapp.js
@@ -0,0 +1,27 @@
+(function () {
+    var saveButton = document.querySelector('#save');
+    saveButton.addEventListener('click', function () {
+        function store(location) {
+            var newItem = document.createElement('li');
+            newItem.textContent = ' - ' + location;
+            notes.appendChild(newItem);
+
+            notes.classList.remove('no-items');
+          
+        }
+
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+            console.log('geolocation in navigator\n' + position.coords.latitude + ', ' +
+                    position.coords.longitude);
+                store(position.coords.latitude + ', ' +
+                    position.coords.longitude);
+            }, function (err) {
+                console.error('Failed to get user location', err);
+                store('Could not get your location');
+            }, {"timeout":5000});
+        } else {
+            store('You don\'t have GPS');
+        }
+    });
+})();
